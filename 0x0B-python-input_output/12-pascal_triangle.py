83d631a3e2163907d855029d9436b8c90f36bc32
Selam25-30 <selamawitbirhanu123@gmail.com>
2022-07-05 19:05:21 -0700

    more than 4 files

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""module for p_tri"""
+
+"""Defines a Pascal's Triangle function."""
 
 
 def pascal_triangle(n):
-    """to make this tri"""
-    tri = []
-    line = [1]
-    y = [0]
-    for x in range(max(n, 0)):
-        tri.append(line)
-        line = [l + r for l, r in zip(line + y, y + line)]
-    return tri
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
